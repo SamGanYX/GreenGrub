7920841ad4be6eb3432433eb345194675288cf9d
Caleb West <calebewest@outlook.com>
2025-05-06 14:11:18 -0700

    Added Food.java to represent entire food

---
+++ backend/src/main/java/com/test/fullstack_backend/model/Food.java
@@ -0,0 +1,15 @@
+package com.test.fullstack_backend.model;
+
+public class Food {
+    private FoodNutrition foodNutrition;
+    private FoodImpact foodImpact;
+
+    public FoodNutrition getNutrition() {
+        return foodNutrition;
+    }
+
+    public FoodImpact getImpact() {
+        return foodImpact;
+    }
+}
+
