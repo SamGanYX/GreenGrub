e72461f606c8383cc212c802b578567814fa2fe6
Caleb West <calebewest@outlook.com>
2025-05-06 14:20:54 -0700

    Caleb added a test

--- backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
+++ backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
@@ -1,13 +1,25 @@
 package com.test.fullstack_backend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.test.fullstack_backend.model.FoodNutrition;
 
 @SpringBootTest
 class FullstackBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void calebFoodNutritionTest() {
+		FoodNutrition fi = new FoodNutrition();
+		fi.setId("testId");
+		assertEquals("testId", fi.getId());
+		
 	}
 
 }
