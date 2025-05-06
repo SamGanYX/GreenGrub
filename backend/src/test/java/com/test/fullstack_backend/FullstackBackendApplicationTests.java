f16dc0ca2de1abd20319ea2571659e64b68a2ca9
Caleb West <calebewest@outlook.com>
2025-05-06 14:58:38 -0700

    Added MockBean to backend tests to  make build not fail

--- backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
+++ backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
@@ -4,15 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.test.fullstack_backend.model.FoodNutrition;
+import com.test.fullstack_backend.model.Users;
+import com.test.fullstack_backend.repository.UserRepository; // Adjust this to your actual repository class
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class FullstackBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private UserRepository userRepository; // Replace with your actual repository
+
+    @Test
+    void contextLoads() {
+    }
+
 
 	@Test
 	void calebFoodNutritionTest() {
