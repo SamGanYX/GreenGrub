aacb6a5c672c0418b19a8bc612981ba9594ce5d9
Julie Yu <julie.12.yu@gmail.com>
2025-05-08 17:56:58 -0700

    caa

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: 'react-native',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-.*|@react-native-.*)/)'
-  ]
+    'node_modules/(?!(react-native|@react-native|react-native-.*|@react-native-.*|@react-navigation|@expo|expo|@babel|react|react-dom)/)'
+  ],
 };
