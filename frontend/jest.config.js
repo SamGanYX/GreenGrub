234cfb274b59190e2769df52ee58144e7fccdb22
Julie Yu <julie.12.yu@gmail.com>
2025-05-08 17:31:58 -0700

    will simpler work?

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,18 +1,6 @@
 /** @type {import('jest').Config} */
-module.exports = {
-    preset: 'react-native',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/__tests__/**/*-test.[jt]s?(x)'],
-    transformIgnorePatterns: [
-      'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))',
-    ],
-    testEnvironment: 'node', // Use node environment for tests to prevent memory leaks
-    moduleNameMapper: {
-      // Add any module mappings if needed
-    },
-    verbose: true,
-  };
+const config = {
+  verbose: true,
+};
+
+module.exports = config;
