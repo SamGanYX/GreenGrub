4e8ae88fe81d85bcc1a0c8fde1bfe5b041f2030d
Julie Yu <julie.12.yu@gmail.com>
2025-05-08 18:17:14 -0700

    easer?

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,9 +1,1 @@
-// jest-setup.js
-// Mock React Native components
-jest.mock('react-native', () => ({
-    Text: function(props) { return props.children; },
-    View: function(props) { return props.children; },
-  }));
-  
-  // Set up a global mock for useColorScheme hook results
-  global.useColorScheme = () => 'light';
+global.useColorScheme = jest.fn(() => 'light');
