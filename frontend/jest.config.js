62826fcd8db88b4a1033e2e55e81ad4ae2f31e2b
Julie Yu <julie.12.yu@gmail.com>
2025-05-08 17:50:17 -0700

    asdfasdf

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,10 @@
+// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
+  // Remove the setupFilesAfterEnv line or point to a file you create
 };
