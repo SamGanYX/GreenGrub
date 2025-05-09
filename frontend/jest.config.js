dcb56758d80f325e9bc2782faf6005ebe4bf50a0
Julie Yu <44332507+Julie12Yu@users.noreply.github.com>
2025-05-09 09:52:52 -0700

    patching jest into yml (#18)
    * making frontend automated testing work maybe :D
    
    * help
    
    * Add .env to gitignore
    
    * ignore .class
    
    * ignore .class files
    
    * Update .gitignore
    
    * This currently works on the cloud SQL
    
    * Removing application.properties from being tracked
    
    * current newest versions
    
    * refactoring to allow for db information in the right spot
    
    * please work o.o
    
    * guh
    
    * asdfasdf
    
    * e
    
    * e
    
    * db naem change
    
    * fixing frontend testing setup
    
    * make renderer match version
    
    * fixing tyes
    
    * issue with multiple configs
    
    * will simpler work?
    
    * adsf
    
    * alr well new error good ig
    
    * a
    
    * revert
    
    * pls owrk
    
    * yes please
    
    * a pt 250
    
    * asdfasdf
    
    * vas
    
    * im
    
    * asdf
    
    * caa
    
    * whoopes forogt to install
    
    * lol
    
    * xd
    
    * sure
    
    * new error good
    
    * whoops i cant read
    
    * okie
    
    * so CLOSE
    
    * easer?
    
    * Downgrade react-native to 0.78.0
    
    * Add more to transformIgnorePatterns
    
    * Update babel presets
    
    * Fix Frontend CI tests
    
    ---------
    
    Co-authored-by: SamGanYX <samganyx@gmail.com>

---
+++ frontend/jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFiles: ['./jest.setup.js']
+};
---
+++ frontend/jest.setup.js
@@ -0,0 +1,1 @@
+global.useColorScheme = jest.fn(() => 'light');
