150b4b55e604ac6c8178a87b36bdb9a5773d8ce7
Julie Yu <julie.12.yu@gmail.com>
2025-05-08 18:02:58 -0700

    xd

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@expo|expo-.*|react-native-.*|@babel/runtime)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
 };
