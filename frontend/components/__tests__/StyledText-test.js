c39757457b525f24d780ad53bba7cd30ea6798cf
Julie Yu <julie.12.yu@gmail.com>
2025-05-13 16:40:32 -0700

    Removing unnecessary change to testing suite

--- frontend/components/__tests__/StyledText-test.js
+++ frontend/components/__tests__/StyledText-test.js
@@ -1,6 +1,3 @@
-// @ts-ignore
-global.FormData = require('react-native/Libraries/Network/FormData');
-
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import { MonoText } from '../StyledText';
