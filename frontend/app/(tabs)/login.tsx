691d01b2e24dab8a122bfa8a78e87ea717417c12
SamGanYX <samganyx@gmail.com>
2025-05-28 12:40:25 -0700

    back to aws

--- frontend/app/(tabs)/login.tsx
+++ frontend/app/(tabs)/login.tsx
@@ -35,7 +35,7 @@ export default function LogInPage() {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://18.191.105.213:8080/login', { username, password });
+      const response = await axios.post('http://18.118.128.63:8080/login', { username, password });
       console.log('Login successful:', response.data);
       await AsyncStorage.setItem('userToken', response.data.token);
       navigation.navigate('camera');
@@ -46,7 +46,7 @@ export default function LogInPage() {
 
   const handleCreateAccount = async () => {
     try {
-      const response = await axios.post('http://18.191.105.213:8080/create_account', { username, password });
+      const response = await axios.post('http://18.118.128.63:8080/create_account', { username, password });
       console.log("Account created successfully");
       await AsyncStorage.setItem('userToken', response.data.token);
       navigation.navigate('camera');
