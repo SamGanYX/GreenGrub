9a76e28b1f1dd5c70213816aa9ae3b30dec0eca4
Julie Yu <julie.12.yu@gmail.com>
2025-05-30 14:02:22 -0700

    Adding jsonPath back, it's now used on Main!

--- backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
+++ backend/src/test/java/com/test/fullstack_backend/FullstackBackendApplicationTests.java
@@ -20,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
