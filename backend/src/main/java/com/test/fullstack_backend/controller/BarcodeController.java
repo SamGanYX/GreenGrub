d68176e477e3d3504fd409934bd98ff3b330c2c9
Julie Yu <julie.12.yu@gmail.com>
2025-05-29 17:30:56 -0700

    Removed: unused imports

--- backend/src/main/java/com/test/fullstack_backend/controller/BarcodeController.java
+++ backend/src/main/java/com/test/fullstack_backend/controller/BarcodeController.java
@@ -5,7 +5,6 @@ import com.test.fullstack_backend.repository.BarcodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
