6c1e6453b9da442a539be06ea988a6f252b0681e
Julie Yu <julie.12.yu@gmail.com>
2025-05-29 19:44:14 -0700

    Changed global data to be map to match new planned schema

--- frontend/app/datashare.tsx
+++ frontend/app/datashare.tsx
@@ -1,23 +1,28 @@
 import React, { ReactNode, createContext, useContext, useState } from 'react';
 
 interface DataShareType {
-    data: string[];
-    setData: React.Dispatch<React.SetStateAction<string[]>>;
-  }
-  
+    data: Map<string, string>;
+    setData: React.Dispatch<React.SetStateAction<Map<string, string>>>;
+}
+
 const DataContext = createContext<DataShareType>({
-    data: [],
+    data: new Map(),
     setData: () => {}
 });
 
-export const DataShare = ({ children } : {children : ReactNode }) => {
-  const [data, setData] = useState<string[]>(['skdibidi', 'skdibidi2']);
-  
-  return (
-    <DataContext.Provider value={{ data, setData }}>
-      {children}
-    </DataContext.Provider>
-  );
+export const DataShare = ({ children }: { children: ReactNode }) => {
+    const [data, setData] = useState<Map<string, string>>(
+        new Map([ // maps the BARCODE to the NAME
+            ['420420420420', 'skdibidi'],
+            ['123123123123', 'skdibidi2']
+        ])
+    );
+
+    return (
+        <DataContext.Provider value={{ data, setData }}>
+            {children}
+        </DataContext.Provider>
+    );
 };
 
 export const useFoodData = () => useContext(DataContext);
