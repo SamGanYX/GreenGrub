098224b7a1b1b25f4175eddf4997526126625ca2
SamGanYX <samganyx@gmail.com>
2025-06-02 19:11:08 -0700

    Update Camera-tests.js

--- frontend/components/__tests__/Camera-tests.js
+++ frontend/components/__tests__/Camera-tests.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import App from 'frontend/app/(tabs)/camera.tsx';
 
 // Mock the necessary modules
 jest.mock('expo-camera', () => ({
-    CameraView: ({ children, onBarcodeScanned }) => (
-      <div onClick={() => onBarcodeScanned && onBarcodeScanned({ type: 'ean13', data: '1234567890123' })}>
-        {children}
-      </div>
-    ),
-    CameraType: {
-      back: 'back',
-      front: 'front',
-    },
-    // Set a default return value for the mock function
-    useCameraPermissions: jest.fn().mockReturnValue([{ granted: true }, jest.fn()]),
-  }));
+  CameraView: ({ children, onBarcodeScanned }) => (
+    <div onClick={() => onBarcodeScanned && onBarcodeScanned({ type: 'ean13', data: '1234567890123' })}>
+      {children}
+    </div>
+  ),
+  CameraType: {
+    back: 'back',
+    front: 'front',
+  },
+  // Set a default return value for the mock function
+  useCameraPermissions: jest.fn().mockReturnValue([{ granted: true }, jest.fn()]),
+}));
 
 jest.mock('expo-constants', () => ({
   expoConfig: {
@@ -36,19 +37,31 @@ describe('App Component', () => {
 
   it('renders correctly when permissions are loading', () => {
     require('expo-camera').useCameraPermissions.mockReturnValueOnce([null, jest.fn()]);
-    const { toJSON } = render(<App />);
+    const { toJSON } = render(
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
+    );
     expect(toJSON()).toMatchSnapshot();
   });
 
   it('renders correctly when permission is not granted', () => {
     require('expo-camera').useCameraPermissions.mockReturnValueOnce([{ granted: false }, jest.fn()]);
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
+    );
     expect(getByText('We need your permission to show the camera')).toBeTruthy();
   });
 
   it('renders camera view correctly when permission is granted', () => {
     require('expo-camera').useCameraPermissions.mockReturnValueOnce([{ granted: true }, jest.fn()]);
-    const { toJSON } = render(<App />);
+    const { toJSON } = render(
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
+    );
     expect(toJSON()).toMatchSnapshot();
   });
 });
